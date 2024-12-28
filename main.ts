8b74315a569631ca8f8acc772f3baf878c602a01
Sebastian Baroni <laantorchaweb@gmail.com>
2024-12-28 14:53:51 -0300

    add plugin files

--- main.ts
+++ main.ts
@@ -1,134 +1,208 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice, PluginSettingTab, Setting, Modal, App } from "obsidian";
+import * as fs from "fs";
+import * as path from "path";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CloneVaultPluginSettings {
+  defaultVaultLocation: string;
+  foldersToCopy: string; // Comma or line-separated
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CloneVaultPluginSettings = {
+  defaultVaultLocation: "",
+  foldersToCopy: ""
+};
+
+class NamePromptModal extends Modal {
+  vaultName: string;
+  onSubmit: (vaultName: string) => void;
+
+  constructor(app: App, onSubmit: (vaultName: string) => void) {
+    super(app);
+    this.onSubmit = onSubmit;
+    this.vaultName = "";
+  }
+
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.createEl("h2", { text: "New Vault Name" });
+
+    new Setting(contentEl)
+      .setName("Vault Name")
+      .addText((text) =>
+        text.onChange((value) => {
+          this.vaultName = value;
+        })
+      );
+
+    new Setting(contentEl).addButton((btn) =>
+      btn
+        .setButtonText("Create")
+        .setCta()
+        .onClick(() => {
+          this.close();
+          this.onSubmit(this.vaultName);
+        })
+    );
+  }
+
+  onClose() {
+    this.contentEl.empty();
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CloneVaultPlugin extends Plugin {
+  settings: CloneVaultPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new CloneVaultPluginSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.addCommand({
+      id: "clone-vault-with-name",
+      name: "Clone Vault (Prompt for name)",
+      callback: async () => {
+        const modal = new NamePromptModal(this.app, async (vaultName: string) => {
+          if (!vaultName) {
+            new Notice("No vault name provided!");
+            return;
+          }
+          try {
+            const currentVaultPath = (this.app.vault.adapter as any).basePath;
+            const newVaultPath = path.join(
+              this.settings.defaultVaultLocation,
+              vaultName
+            );
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+            await copyObsidianFolder(currentVaultPath, newVaultPath);
+            await copyFolderStructure(
+              currentVaultPath,
+              newVaultPath,
+              this.settings.foldersToCopy
+            );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+            new Notice(`Vault "${vaultName}" cloning successful!`);
+          } catch (err) {
+            console.error(err);
+            new Notice("Vault cloning failed!");
+          }
+        });
+        modal.open();
+      },
+    });
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+  async onunload() {}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CloneVaultPluginSettingTab extends PluginSettingTab {
+  plugin: CloneVaultPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: CloneVaultPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  display() {
+    const { containerEl } = this;
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName("Default Vault Location")
+      .setDesc("Base folder where new vaults will be created.")
+      .addText((text) =>
+        text
+          .setPlaceholder("/Users/username/Documents/ObsidianVaults")
+          .setValue(this.plugin.settings.defaultVaultLocation)
+          .onChange(async (value) => {
+            this.plugin.settings.defaultVaultLocation = value;
+            await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName("Folders to Copy with Files")
+      .setDesc("Comma or line-separated folder names to copy entirely.")
+      .addTextArea((textArea) =>
+        textArea
+          .setPlaceholder("templates, media, etc.")
+          .setValue(this.plugin.settings.foldersToCopy)
+          .onChange(async (value) => {
+            this.plugin.settings.foldersToCopy = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+async function copyObsidianFolder(currentVaultPath: string, newVaultPath: string) {
+  const oldConfigFolder = path.join(currentVaultPath, ".obsidian");
+  const newConfigFolder = path.join(newVaultPath, ".obsidian");
+  if (!fs.existsSync(oldConfigFolder)) return;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  fs.mkdirSync(newVaultPath, { recursive: true });
+  fs.mkdirSync(newConfigFolder, { recursive: true });
 
-	display(): void {
-		const {containerEl} = this;
+  const items = fs.readdirSync(oldConfigFolder);
+  for (const item of items) {
+    const src = path.join(oldConfigFolder, item);
+    const dest = path.join(newConfigFolder, item);
+    const stats = fs.statSync(src);
 
-		containerEl.empty();
+    if (stats.isDirectory()) {
+      fs.mkdirSync(dest, { recursive: true });
+      copyDirectory(src, dest);
+    } else {
+      fs.copyFileSync(src, dest);
+    }
+  }
+}
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function copyDirectory(src: string, dest: string) {
+  const items = fs.readdirSync(src);
+  for (const item of items) {
+    const s = path.join(src, item);
+    const d = path.join(dest, item);
+    const stats = fs.statSync(s);
+
+    if (stats.isDirectory()) {
+      fs.mkdirSync(d, { recursive: true });
+      copyDirectory(s, d);
+    } else {
+      fs.copyFileSync(s, d);
+    }
+  }
+}
+
+async function copyFolderStructure(
+  currentVaultPath: string,
+  newVaultPath: string,
+  foldersToCopyStr: string
+) {
+  const foldersToCopy = foldersToCopyStr
+    .split(/[\n,]+/)
+    .map((f) => f.trim())
+    .filter(Boolean);
+
+  const entries = fs.readdirSync(currentVaultPath, { withFileTypes: true });
+  for (const entry of entries) {
+    if (!entry.isDirectory() || entry.name === ".obsidian") continue;
+
+    const folderPath = path.join(currentVaultPath, entry.name);
+    const newFolderPath = path.join(newVaultPath, entry.name);
+    fs.mkdirSync(newFolderPath, { recursive: true });
+
+    if (foldersToCopy.includes(entry.name)) {
+      copyDirectory(folderPath, newFolderPath);
+    } else {
+      await copyFolderStructure(folderPath, newFolderPath, foldersToCopyStr);
+    }
+  }
 }
